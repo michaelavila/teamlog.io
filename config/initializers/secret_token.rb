f44689c69e3e7b14820a70ec79d4f374792a6193
Michael Avila <me@michaelavila.com>
2013-08-11 16:34:45 -0700

    Add initial rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamlogIo::Application.config.secret_token = '0bef200a08bf3efff6a1188befb250753c76a1f630bbfd696b69bcd6eb2d98eeac310e85aa704cbd3fe8ca3a68cb6db6364505b2ee3b2177f124b97692e65b87'
